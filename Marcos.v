6bef2a2462ea0b2fb18c77e3660294814d5c892f
Sebas Quesada <s_quesada96@hotmail.com>
2017-04-01 19:30:46 -0600

    Cambios hechos
    A la fontrom le quite algunas letras que no ibamos a usar y agregue las
    que sí vamos usar, al igual que acomode los selec_character de cada uno

--- Marcos.v
+++ Marcos.v
@@ -1,30 +1,14 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/30/2017 01:37:40 PM
-// Design Name: 
-// Module Name: Marcos
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module Marcos(
  	input wire rst, //reset
  	input wire video_on, // indica donde pintar
  	input wire [9:0] pixel_x, pixel_y, // posicion coordenadas x, y
- 	output [11:0] rgb_text // salida de colores
+ 	output [3:0] ro,go,bo,
+ 	output [7:0] font1,
+ 	output [2:0] char1,
+ 	output fb
 );
+
+wire fhv1,fhv2, fhh1, fhh2, cajitafh, hv1, hv2, hh1, hh2, cajitah, tv1, tv2, th1, th2, cajitat;
 
     // BORDE DE PANTALLA    
 
@@ -36,8 +20,8 @@ module Marcos(
 	localparam BordePantallay    =  0;
 	localparam BordePantallayf   = 479;
 	wire  BordeIzq_on;
-	assign BordeIzq_on = (BordePantallaxIzq<=pixel_x) && (pixel_x<=BordePantallaxDer) &&
-	(BordePantallay<=pixel_y) && (pixel_y<=BordePantallayf);
+	assign BordeIzq_on = ((pixel_x>=BordePantallaxIzq) && (pixel_x<=BordePantallaxDer) &&
+        (pixel_y>=BordePantallay) && (pixel_y<=BordePantallayf));
 
 	// SEGUNDO BORDE DE PANTALLA 
 	localparam BordePantalla2xIzq=629;
@@ -45,8 +29,8 @@ module Marcos(
 	localparam BordePantalla2y  = 0;
 	localparam BordePantalla2yf  = 479;
 	wire  BordeDer_on;
-	assign BordeDer_on = (BordePantalla2xIzq<=pixel_x) && (pixel_x<=BordePantalla2xDer) &&
-	(BordePantalla2y <=pixel_y) && (pixel_y<=BordePantalla2yf);
+	assign BordeDer_on = ((pixel_x>=BordePantalla2xIzq) && (pixel_x<=BordePantalla2xDer) &&
+        (pixel_y>=BordePantalla2y) && (pixel_y<=BordePantalla2yf));
 
 // ******************************************************	
 
@@ -58,8 +42,8 @@ module Marcos(
 	localparam BordeSupy = 0;
 	localparam BordeSupyf = 10;
 	wire  BordeSup_on;
-	assign BordeSup_on = (BordeSupIzqx<=pixel_x) && (pixel_x<=BordeSupDerx) &&
-	(BordeSupy<=pixel_y) && (pixel_y<=BordeSupyf);
+	assign BordeSup_on = ((pixel_x>=BordeSupIzqx) && (pixel_x<=BordeSupDerx) &&
+        (pixel_y>=BordeSupy) && (pixel_y<=BordeSupyf));
 	
 	// SEGUNDO BORDE DE PANTALLA
 	localparam BordeInfIzqx=10;
@@ -67,9 +51,146 @@ module Marcos(
 	localparam BordeInfy = 469;
 	localparam BordeInfyf = 479;
 	wire  BordeInf_on;
-	assign BordeInf_on = (BordeInfIzqx<=pixel_x) && (pixel_x<=BordeInfDerx) &&
-	(BordeInfy<=pixel_y) && (pixel_y<=BordeInfyf);
+	assign BordeInf_on = ((pixel_x>=BordeInfIzqx) && (pixel_x<=BordeInfDerx) &&
+	(pixel_y>=BordeInfy) && (pixel_y<=BordeInfyf));
+	
+	//Cajita para fecha
+	assign fhv1 = ((pixel_x>=160)&&(pixel_x<=175)&&(pixel_y>=32)&&(pixel_y<=127));
+	assign fhv2 = ((pixel_x>=448)&&(pixel_x<=463)&&(pixel_y>=32)&&(pixel_y<=127));
+	assign fhh1 = ((pixel_x>=160)&&(pixel_x<=463)&&(pixel_y>=32)&&(pixel_y<=47));
+	assign fhh2 = ((pixel_x>=160)&&(pixel_x<=463)&&(pixel_y>=112)&&(pixel_y<=127));
+	assign cajitafh = (fhv1|fhv2|fhh1|fhh2);
+	
+	//Cajita para hora
+    assign hv1 = ((pixel_x>=128)&&(pixel_x<=144)&&(pixel_y>=192)&&(pixel_y<=287));
+    assign hv2 = ((pixel_x>=480)&&(pixel_x<=496)&&(pixel_y>=192)&&(pixel_y<=287));
+    assign hh1 = ((pixel_x>=128)&&(pixel_x<=496)&&(pixel_y>=192)&&(pixel_y<=207));
+    assign hh2 = ((pixel_x>=128)&&(pixel_x<=496)&&(pixel_y>=272)&&(pixel_y<=287));
+    assign cajitah = (hv1|hv2|hh1|hh2);
+    
+    //Cajita para timer
+    assign tv1 = ((pixel_x>=160)&&(pixel_x<=175)&&(pixel_y>=336)&&(pixel_y<=431));
+    assign tv2 = ((pixel_x>=448)&&(pixel_x<=463)&&(pixel_y>=336)&&(pixel_y<=431));
+    assign th1 = ((pixel_x>=160)&&(pixel_x<=463)&&(pixel_y>=336)&&(pixel_y<=351));
+    assign th2 = ((pixel_x>=160)&&(pixel_x<=463)&&(pixel_y>=416)&&(pixel_y<=431));
+    assign cajitat = (tv1|tv2|th1|th2);
 
-// ******************************************************	
+//letras
+reg [1:0] char;
+wire [3:0] row;
+wire [5:0] rom;
+reg [2:0] selec;
+wire [2:0] bit;
+wire [7:0] font;
+wire fontb;
 
+fontROM rom1(.clk(clk),
+             .addr(rom),
+             .sel_caracter(selec),
+             .data(font));
+wire fok,hok,h1ok,rok,tok,iok, varaok;
+assign row = pixel_y[3:0];
+assign bit = pixel_x[2:0];
+assign rom = {char,row};
+assign fontb=font[~bit];
+assign fok=((pixel_x>=96)&&(pixel_x<=111)&&(pixel_y>=48)&&(pixel_y<=79));
+assign hok=((pixel_x>=128)&&(pixel_x<=143)&&(pixel_y>=48)&&(pixel_y<=79));
+assign font1=font;
+assign fb=fontb;
+
+	
+// ***************Multiplexor de bordes*****************
+reg [3:0]r;
+reg [3:0]g;
+reg [3:0]b;
+/*always @*
+begin
+    if(fok)
+    begin
+    selec<=1'b0;
+    char<=2'b01;
+            if(fontb==1)
+            begin
+                r<=4'h0;
+                g<=4'h0;
+                b<=4'h1;
+            end
+            else
+            begin
+                r<=4'h0;
+                g<=4'h0;
+                b<=4'h1;
+            end
+      end
+    else if(hok) 
+    begin
+    selec<=1'b1;
+    char<=2'b10;
+        if(fontb==1)
+        begin
+            r<=4'h0;
+            g<=4'h0;
+            b<=4'h1;
+        end
+        else
+        begin
+            r<=4'h0;
+            g<=4'h0;
+            b<=4'h1;
+        end
+    end
+    else
+    begin
+        r<=4'h1;
+        g<=4'h1;
+        b<=4'h1;
+    end
+end*/
+always @*
+    if(rst|~video_on)
+        begin
+            r<=1;
+            g<=1;
+            b<=1;
+        end
+    else
+        begin
+            if(BordeIzq_on|BordeDer_on|BordeSup_on|BordeInf_on)
+                begin
+                    r<=4'h5;
+                    g<=4'h0;
+                    b<=4'h3;
+                end
+            else if(cajitafh)
+                    begin
+                       r<=4'h0;
+                       g<=4'h0;
+                       b<=4'h1;  
+                    end
+            else if(cajitah)
+                    begin
+                        r<=4'h0;
+                        g<=4'h1;
+                        b<=4'h0;  
+                    end
+            else if(cajitat)
+                    begin
+                        r<=4'h1;
+                        g<=4'h0;
+                        b<=4'h0;  
+                    end
+            else
+                begin
+                    r<=4'h1;
+                    g<=4'h1;
+                    b<=4'h1;
+                end
+        end
+
+ assign ro=r;
+ assign go=g;
+ assign bo=b;
+ assign char1=char;
 endmodule
+
+
