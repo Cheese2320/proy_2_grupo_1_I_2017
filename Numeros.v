c6a3f397ab72a6fb12712f040d13bb1d6491b1cd
MIVR1296 <kape1296@gmail.com>
2017-04-06 21:27:10 -0600

    Correcion

--- Numeros.v
+++ Numeros.v
@@ -64,12 +64,12 @@ DECO Dec1(.enable(enable), .bcd_num(bcd_deco), .address_out(char2));
 wire dia1,dia2, mes1,mes2,a1,a2; // Cifras de los dias, meses y años
 
 // Se asigna el valor a cada cifra segun los 8 bits entrantes
-assign dia1=  
-assign dia2= 
-assign mes1= 
-assign mes1=
-assign a1= 
-assign a2= 
+assign dia1= ((pixel_x>=192)&&(pixel_x<=207)&&(pixel_y>=64)&&(pixel_y<=95));
+assign dia2= ((pixel_x>=220)&&(pixel_x<=235)&&(pixel_y>=64)&&(pixel_y<=95));
+assign mes1= ((pixel_x>=220)&&(pixel_x<=235)&&(pixel_y>=64)&&(pixel_y<=95));
+assign mes1= ((pixel_x>=220)&&(pixel_x<=235)&&(pixel_y>=64)&&(pixel_y<=95));
+assign a1=   ((pixel_x>=220)&&(pixel_x<=235)&&(pixel_y>=64)&&(pixel_y<=95));
+assign a2=   ((pixel_x>=220)&&(pixel_x<=235)&&(pixel_y>=64)&&(pixel_y<=95));
 
 reg [3:0]r;
 reg [3:0]g;
@@ -77,34 +77,35 @@ reg [3:0]b;
 
 always @*
     begin
-	   if(dia1) //
+	   if(dia1)
             begin
-                bcd_to_deco<=fechad[7:4];
-                enable<=1;
+                bcd_deco<=fechad[7:4];
+                enablereg<=1;
             end
        else if (dia2)
             begin
-                bcd_to_deco<=fechad[3:0];
-                enable<=1;
+                bcd_deco<=fechad[3:0];
+                enablereg<=1;
 	        end
 	   else if (mes1)
             begin
-                bcd_to_deco<=fecham[7:4];
-                enable<=1;
+                bcd_deco<=fecham[7:4];
+                enablereg<=1;
 	        end
 	   else if (mes2)
             begin
-		        bcd_to_deco<=fecham[3:0];
-                enable<=1; 
+		        bcd_deco<=fecham[3:0];
+                enablereg<=1; 
 	        end
 	   else if (a1)
-                bcd_to_deco<=fechaa[7:4];
-                enable<=1; 
+	       begin
+                bcd_deco<=fechaa[7:4];
+                enablereg<=1; 
 	        end
 	   else if (a2)
 	        begin
-                bcd_to_deco<=fechaa[3:0];
-                enable<=1;
+                bcd_deco<=fechaa[3:0];
+                enablereg<=1;
 	        end
         else 
         char2<=5'b00000; // fondo
